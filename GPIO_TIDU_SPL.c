0b121f63f3610d0484fc439accf252f3ffe438ed
Tidu09 <45685841+Tidu09@users.noreply.github.com>
2019-05-26 17:00:37 +0530

    Stm Gpio code with and without library

---
+++ GPIO_TIDU_SPL.c
@@ -0,0 +1,47 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f30x.h"
+#include "stm32f3_discovery.h"
+
+
+#define LED    GPIO_Pin_9
+#define LED1   GPIO_Pin_8
+
+int main()
+{
+
+      GPIO_InitTypeDef Gpio;
+
+
+    RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOE, ENABLE );
+
+
+      // Configure port E (LED)
+      GPIO_StructInit( &Gpio );
+
+      Gpio.GPIO_Mode = GPIO_Mode_OUT;
+      Gpio.GPIO_Pin  = LED;                    // pin 9
+      Gpio.GPIO_Pin |= LED1;                // pin 8
+      Gpio.GPIO_Speed = GPIO_Speed_50MHz ;
+      Gpio.GPIO_PuPd= GPIO_PuPd_DOWN;
+
+      GPIO_Init( GPIOE, &Gpio );
+
+
+      while(1)
+      {
+              GPIO_SetBits( GPIOE, LED );
+
+              GPIO_SetBits( GPIOE, LED1 );
+
+      }
+}
---
+++ GPIO_Tidu.c
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f30x.h"
+#include "stm32f3_discovery.h"
+			
+
+int main(void)
+{  long i;
+   RCC->AHBENR |=(1<<21);
+   GPIOE ->MODER |= (1<<16);
+   GPIOE->PUPDR |= (1<<17);
+   GPIOE -> OSPEEDR|=(1<<16)|(1<<17);
+
+
+   while(1)
+   {
+	   for(i=0;i<500000;i++)
+		   GPIOE ->BSRR = (1<<8);
+	   for(i=0;i<500000;i++)
+		   GPIOE ->BSRR =(1<<24);
+   }
+}
